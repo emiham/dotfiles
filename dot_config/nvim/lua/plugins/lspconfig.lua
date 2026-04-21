fbd4257c5ac73875057c14b37f62ddfb59728cf3
Emil Hammarberg <emilhammarberg@mailbox.org>
2026-04-21 14:06:49 +0200

    Add gopls

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -241,6 +241,27 @@ return {
             },
           },
         },
+        {
+          "gopls",
+          {
+            settings = {
+              gopls = {
+                semanticTokens = true,
+                analyses = { unusedparams = true },
+                staticcheck = true,
+                hints = {
+                  assignVariableTypes = true,
+                  compositeLiteralFields = true,
+                  compositeLiteralTypes = true,
+                  constantValues = true,
+                  functionTypeParameters = true,
+                  parameterNames = true,
+                  rangeVariableTypes = true,
+                },
+              },
+            },
+          },
+        },
       }
 
       for _, lsp in pairs(lsps) do
