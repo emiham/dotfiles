5da71b6054f79e13d5d95699fa3c9e028d35ce19
Emil Hammarberg <emilhammarberg@mailbox.org>
2026-04-27 16:04:28 +0200

    Don't hardcode colors

--- dot_config/nvim/lua/plugins/highlight-colors.lua
+++ dot_config/nvim/lua/plugins/highlight-colors.lua
@@ -6,6 +6,17 @@ return {
       formatting = {
         format = require("nvim-highlight-colors").format,
       },
+      custom_colors = {
+        -- { label = "white", color = "#d0d0d0" },
+        -- { label = "black", color = "#4e4e4e" },
+        -- { label = "red", color = "#d68787" },
+        -- { label = "green", color = "#5f865f" },
+        -- { label = "yellow", color = "#d8af5f" },
+        -- { label = "blue", color = "#85add4" },
+        -- { label = "magenta", color = "#d7afaf" },
+        -- { label = "cyan", color = "#87afaf" },
+        -- { label = "white", color = "#d0d0d0" },
+      },
     }
   end,
 }
