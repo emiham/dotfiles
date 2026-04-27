d22053fe8d4c592d74dbe4bc19cc08b393b4f060
Emil Hammarberg <emilhammarberg@mailbox.org>
2026-04-27 20:30:59 +0200

    Remove Lazy plugins

--- dot_config/nvim/lua/plugins/quicker.lua
+++
@@ -1,8 +0,0 @@
-return {
-  -- TODO Add mappings to navigate quickfix
-  "stevearc/quicker.nvim",
-  event = "FileType qf",
-  ---@module "quicker"
-  ---@type quicker.SetupOptions
-  opts = {},
-}
--- dot_config/nvim/lua/plugins/syntax.lua
+++
@@ -1,8 +0,0 @@
-return {
-  { "freitass/todo.txt-vim" },
-  { "PontusHanssen/pddl.vim" },
-  { "vim-scripts/icalendar.vim" },
-  { "mustache/vim-mustache-handlebars" },
-  { "phelipetls/vim-hugo" },
-  { "https://gitlab.com/ceda_ei/vcf.vim.git" },
-}
