a3b3524d6530b5f7a1342d99ade6c57f4a4bcaba
Emil Hammarberg <emilhammarberg@mailbox.org>
2026-04-27 15:40:28 +0200

    Use file extension for MIME type
    I'd like to only do this for NFS, or to do this first and then run
    mime.local in the background for more accuracy.

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -16,3 +16,21 @@ Status:children_add(function()
     })
   end
 end, 500, Status.RIGHT)
+
+require("mime-ext.local"):setup({
+  -- Expand the existing filename database (lowercase), for example:
+  with_files = {
+    makefile = "text/makefile",
+    -- ...
+  },
+
+  -- Expand the existing extension database (lowercase), for example:
+  with_exts = {
+    mk = "text/makefile",
+    -- ...
+  },
+
+  -- If the MIME type is not in both filename and extension databases,
+  -- then fallback to Yazi's preset `mime.local` plugin, which uses `file(1)`
+  fallback_file1 = false,
+})
