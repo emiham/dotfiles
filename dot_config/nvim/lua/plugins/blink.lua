8c72032c3ad6db40a3e223d982e6126349685a4a
Emil Hammarberg <emilhammarberg@mailbox.org>
2026-04-27 16:03:52 +0200

    Set up colorful icons

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -24,7 +24,6 @@ return {
       keymap = {
         preset = "cmdline",
       },
-
       completion = {
         list = {
           selection = {
@@ -167,7 +166,45 @@ return {
         auto_show = true,
         auto_show_delay_ms = 500,
       },
-      menu = {},
+      menu = {
+        draw = {
+          components = {
+            -- customize the drawing of kind icons
+            kind_icon = {
+              text = function(ctx)
+                -- default kind icon
+                local icon = ctx.kind_icon
+                -- if LSP source, check for color derived from documentation
+                if ctx.item.source_name == "LSP" then
+                  local color_item = require("nvim-highlight-colors").format(
+                    ctx.item.documentation,
+                    { kind = ctx.kind }
+                  )
+                  if color_item and color_item.abbr ~= "" then
+                    icon = color_item.abbr
+                  end
+                end
+                return icon .. ctx.icon_gap
+              end,
+              highlight = function(ctx)
+                -- default highlight group
+                local highlight = "BlinkCmpKind" .. ctx.kind
+                -- if LSP source, check for color derived from documentation
+                if ctx.item.source_name == "LSP" then
+                  local color_item = require("nvim-highlight-colors").format(
+                    ctx.item.documentation,
+                    { kind = ctx.kind }
+                  )
+                  if color_item and color_item.abbr_hl_group then
+                    highlight = color_item.abbr_hl_group
+                  end
+                end
+                return highlight
+              end,
+            },
+          },
+        },
+      },
     },
   },
   -- allows extending the providers array elsewhere in your config
