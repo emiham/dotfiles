32f5c3c19453b3d6f1354887aea89359c85e6a34
Emil Hammarberg <emilhammarberg@mailbox.org>
2026-06-02 16:30:01 +0200

    Add script to open video on Youtube

---
+++ dot_config/mpv-float/scripts/open_on_youtube.lua
@@ -0,0 +1,22 @@
+local function open_youtube_url()
+  local filename = mp.get_property("filename")
+
+  local id = string.match(filename, "%[(.-)%]")
+
+  if id and #id == 11 then
+    local url = "https://www.youtube.com/watch?v=" .. id
+    mp.msg.info("Opening URL: " .. url)
+    local platform = mp.get_property("platform")
+    if platform == "windows" then
+      mp.commandv("run", "cmd", "/c", "start", url)
+    elseif platform == "darwin" then
+      mp.commandv("run", "open", url)
+    else
+      mp.commandv("run", "xdg-open", url)
+    end
+  else
+    mp.osd_message("No valid YouTube ID found in brackets")
+  end
+end
+
+mp.add_key_binding("o", "open-youtube-from-id", open_youtube_url)
