4ab0aed0ec4a3e4540d8396537cd0ce71915403d
Emil Hammarberg <emilhammarberg@mailbox.org>
2026-06-02 16:52:57 +0200

    Make lua_ls a lot less verbose

--- dot_config/nvim/plugin/fidget.nvim.lua
+++ dot_config/nvim/plugin/fidget.nvim.lua
@@ -4,4 +4,22 @@ require("fidget").setup({
   notification = {
     override_vim_notify = true,
   },
+  progress = {
+    ignore = {
+      function(msg)
+        if msg.lsp_client.name == "lua_ls" then
+          if
+            msg.title == "Diagnosing"
+            or msg.title == "Processing file symbols..."
+            or msg.title == "Processing completion..."
+            or msg.title == "Processing full semantic tokens..."
+            or msg.title == "Processing incremental semantic tokens..."
+          then
+            return true
+          end
+        end
+        return false
+      end,
+    },
+  },
 })
