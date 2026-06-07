500354ebb376a4677bfca25bb6e1223ee2a0a8a7
Emil Hammarberg <emilhammarberg@mailbox.org>
2026-06-07 16:48:13 +0200

    Increase match range

--- dot_config/nvim/plugin/mini.ai.lua
+++ dot_config/nvim/plugin/mini.ai.lua
@@ -1,3 +1,12 @@
 vim.pack.add({ "https://github.com/nvim-mini/mini.ai" })
 
-require("mini.ai").setup()
+require("mini.ai").setup({
+  custom_textobjects = {
+    e = { "()()%f[%w-][%w-]+()[ \t]*()" },
+    t = require("mini.ai").gen_spec.treesitter({
+      a = "@function.outer",
+      i = "@function.inner",
+    }),
+  },
+  n_lines = 500,
+})
