1f84e2105be1f9a1b50945a11ced8549214e6e34
Emil Hammarberg <emilhammarberg@mailbox.org>
2026-07-12 17:24:55 +0200

    Clean up/fix yazi wrapper

--- dot_config/xdg-desktop-portal-termfilechooser/executable_yazi-wrapper.sh
+++ dot_config/xdg-desktop-portal-termfilechooser/executable_yazi-wrapper.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -x
-
 # This wrapper script is invoked by xdg-desktop-portal-termfilechooser.
 #
 # Inputs:
@@ -27,35 +25,41 @@ directory="$2"
 save="$3"
 path="$4"
 out="$5"
+debug="$6"
+
+set -e
+
+if [ "$debug" = 1 ]; then
+  set -x
+fi
+
 cmd="yazi"
-# "wezterm start --always-new-process" if you use wezterm
 termcmd="${TERMCMD:-foot}"
+
 # change this to "/tmp/xxxxxxx/.last_selected" if you only want to save last selected location
 # in session (flushed after reset device)
 last_selected_path_cfg="$XDG_DATA_HOME/.last_selected"
 mkdir -p "$(dirname last_selected_path_cfg)"
 if [ ! -f "$last_selected_path_cfg" ]; then
-    touch "$last_selected_path_cfg"
+  touch "$last_selected_path_cfg"
 fi
 last_selected="$(cat "$last_selected_path_cfg")"
 
-# Restore last selected path
 if [ -d "$last_selected" ]; then
-    save_to_file=""
-    if [ "$save" = "1" ]; then
-        save_to_file="$(basename "$path")"
-        path="${last_selected}/${save_to_file}"
-    else
-        path="${last_selected}"
-    fi
+  save_to_file=""
+  if [ "$save" = "1" ]; then
+    save_to_file="$(basename "$path")"
+    path="${last_selected}/${save_to_file}"
+  else
+    path="${last_selected}"
+  fi
 fi
 if [[ -z "$path" ]]; then
-    path="$HOME"
+  path="$HOME"
 fi
 
 if [ "$save" = "1" ]; then
-    # Save/download file
-    printf '%s' 'xdg-desktop-portal-termfilechooser saving files tutorial
+  printf '%s' 'xdg-desktop-portal-termfilechooser saving files tutorial
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!                 === WARNING! ===                 !!!
@@ -76,16 +80,13 @@ Notes:
 2) If you quit ranger without opening a file, this file
    will be removed and the save operation aborted.
 ' >"$path"
-    set -- --chooser-file="$out" --cwd-file="$last_selected_path_cfg" "$path"
+  set -- --chooser-file="$out" --cwd-file="$last_selected_path_cfg" "$path"
 elif [ "$directory" = "1" ]; then
-    # upload files from a directory
-    set -- --cwd-file="$out" "$path"
+  set -- --cwd-file="$out" "$path"
 elif [ "$multiple" = "1" ]; then
-    # upload multiple files
-    set -- --chooser-file="$out" --cwd-file="$last_selected_path_cfg" "$path"
+  set -- --chooser-file="$out" --cwd-file="$last_selected_path_cfg" "$path"
 else
-    # upload only 1 file
-    set -- --chooser-file="$out" --cwd-file="$last_selected_path_cfg" "$path"
+  set -- --chooser-file="$out" --cwd-file="$last_selected_path_cfg" "$path"
 fi
 
 $termcmd -- $cmd "$@"
@@ -93,10 +94,10 @@ $termcmd -- $cmd "$@"
 # Save the last selected path for the next time, only upload files from a directory operation is need
 # because `--cwd-file` will do the same thing for files(s) upload and download operations
 if [ "$save" = "0" ] && [ "$directory" = "1" ]; then
-    echo "$(head -n 1 <"$out")" >"$last_selected_path_cfg"
+  echo "$(head -n 1 <"$out")" >"$last_selected_path_cfg"
 fi
 
 # Remove file if the save operation aborted
 if [ "$save" = "1" ] && [ ! -s "$out" ]; then
-    rm "$path"
+  rm "$path"
 fi
