274d96bc5d7aa9598b829929789a5731ed129b93
Emil Hammarberg <emilhammarberg@mailbox.org>
2026-08-12 11:38:13 +0200

    Update antidote path format

--- dot_config/zsh/dot_zsh_plugins.zsh
+++ dot_config/zsh/dot_zsh_plugins.zsh
@@ -1,20 +1,22 @@
-fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-mattmc3-SLASH-ez-compinit" )
-source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-mattmc3-SLASH-ez-compinit/ez-compinit.plugin.zsh"
-fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-MichaelAquilina-SLASH-zsh-auto-notify" )
-source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-MichaelAquilina-SLASH-zsh-auto-notify/auto-notify.plugin.zsh"
-fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-jeffreytse-SLASH-zsh-vi-mode" )
-source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-jeffreytse-SLASH-zsh-vi-mode/zsh-vi-mode.plugin.zsh"
-fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-agkozak-SLASH-zhooks" )
-source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-agkozak-SLASH-zhooks/zhooks.plugin.zsh"
-fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab" )
-source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-Aloxaf-SLASH-fzf-tab/fzf-tab.plugin.zsh"
-fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-MichaelAquilina-SLASH-zsh-autoswitch-virtualenv" )
-source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-MichaelAquilina-SLASH-zsh-autoswitch-virtualenv/autoswitch_virtualenv.plugin.zsh"
-fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions" )
-source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh"
-fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions" )
-source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh"
-fpath+=( "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting" )
-source "$HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/mattmc3/ez-compinit" )
+source "$HOME/.cache/antidote/github.com/mattmc3/ez-compinit/ez-compinit.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/MichaelAquilina/zsh-auto-notify" )
+source "$HOME/.cache/antidote/github.com/MichaelAquilina/zsh-auto-notify/auto-notify.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/jeffreytse/zsh-vi-mode" )
+source "$HOME/.cache/antidote/github.com/jeffreytse/zsh-vi-mode/zsh-vi-mode.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/agkozak/zhooks" )
+source "$HOME/.cache/antidote/github.com/agkozak/zhooks/zhooks.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/Aloxaf/fzf-tab" )
+source "$HOME/.cache/antidote/github.com/Aloxaf/fzf-tab/fzf-tab.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/MichaelAquilina/zsh-autoswitch-virtualenv" )
+source "$HOME/.cache/antidote/github.com/MichaelAquilina/zsh-autoswitch-virtualenv/autoswitch_virtualenv.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/zsh-users/zsh-autosuggestions" )
+source "$HOME/.cache/antidote/github.com/zsh-users/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/zsh-users/zsh-completions" )
+source "$HOME/.cache/antidote/github.com/zsh-users/zsh-completions/zsh-completions.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/zdharma-continuum/fast-syntax-highlighting" )
+source "$HOME/.cache/antidote/github.com/zdharma-continuum/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
 fpath+=( "$HOME/.cache/antidote/github.com/sindresorhus/pure" )
 source "$HOME/.cache/antidote/github.com/sindresorhus/pure/pure.plugin.zsh"
+fpath+=( "$HOME/.cache/antidote/github.com/junegunn/fzf-git.sh" )
+source "$HOME/.cache/antidote/github.com/junegunn/fzf-git.sh/fzf-git.sh"
