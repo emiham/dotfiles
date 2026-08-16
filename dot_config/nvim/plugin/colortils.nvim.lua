1716c588ccf01b71d65961ab01aee98044c77e08
Emil Hammarberg <emilhammarberg@mailbox.org>
2026-08-16 21:10:29 +0200

    Switch to mini.hipatterns

--- dot_config/nvim/plugin/colortils.nvim.lua
+++
@@ -1,1 +0,0 @@
-vim.pack.add({ "https://github.com/max397574/colortils.nvim" })
---
+++ dot_config/nvim/plugin/mini.hipatterns.lua
@@ -0,0 +1,16 @@
+vim.pack.add({ "https://github.com/nvim-mini/mini.hipatterns" })
+
+local hipatterns = require("mini.hipatterns")
+hipatterns.setup({
+  highlighters = {
+    fixme = {
+      pattern = "%f[%w]()FIXME()%f[%W]",
+      group = "MiniHipatternsFixme",
+    },
+    hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+    todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+    note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+
+    hex_color = hipatterns.gen_highlighter.hex_color(),
+  },
+})
